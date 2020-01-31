4306ef9e01b2505541b1fbfe28650bd5aaf0d352
ahmedalkhashab <a.khashab88@gmail.com>
2020-01-31 23:01:48 +0300

    feat(data): Creating the data layer interface #2

--- Data/src/main/java/io/android/projectx/data/repository/RecipesDataStore.kt
+++ Data/src/main/java/io/android/projectx/data/repository/RecipesDataStore.kt
@@ -8,6 +8,10 @@ interface RecipesDataStore {
 
     fun getRecipes(): Observable<List<RecipeEntity>>
 
+    fun clearRecipes(): Completable
+
+    fun saveRecipes(recipes: List<RecipeEntity>): Completable
+
     fun setRecipeAsBookmarked(recipeId: Long): Completable
 
     fun setRecipeAsNotBookmarked(recipeId: Long): Completable
