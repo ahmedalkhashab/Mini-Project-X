85ac0524da80264ddbd895b099a4de0a2c56c704
ahmedalkhashab <a.khashab88@gmail.com>
2020-01-31 11:43:32 +0300

    feat(domain): add a model Recipe

---
+++ Domain/src/main/java/io/android/projectx/domain/model/Recipe.kt
@@ -0,0 +1,9 @@
+package io.android.projectx.domain.model
+
+import java.util.*
+
+class Recipe(
+    val id: Long, val author: String, val title: String, val description: String,
+    val url: String, val urlToImage: String, val publishedAt: Date,
+    val content: String
+)
