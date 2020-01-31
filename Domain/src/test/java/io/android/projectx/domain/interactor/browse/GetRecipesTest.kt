c33523d3f40bd9215f04417b9194002253f52265
ahmedalkhashab <a.khashab88@gmail.com>
2020-01-31 11:57:15 +0300

    test(domain): test 'GetRecipes' UseCase

---
+++ Domain/src/test/java/io/android/projectx/domain/interactor/browse/GetRecipesTest.kt
@@ -0,0 +1,47 @@
+package io.android.projectx.domain.interactor.browse
+
+import com.nhaarman.mockitokotlin2.whenever
+import io.android.projectx.domain.executor.PostExecutionThread
+import io.android.projectx.domain.model.Recipe
+import io.android.projectx.domain.repository.RecipesRepository
+import io.android.projectx.domain.test.RecipeDataFactory
+import io.reactivex.Observable
+import org.mockito.Mock
+import org.junit.Before
+import org.junit.Test
+import org.mockito.MockitoAnnotations
+
+class GetRecipesTest {
+    private lateinit var getRecipes: GetRecipes
+    @Mock
+    lateinit var recipesRepository: RecipesRepository
+    @Mock
+    lateinit var postExecutionThread: PostExecutionThread
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+        getRecipes = GetRecipes(recipesRepository, postExecutionThread)
+    }
+
+    @Test
+    fun getRecipesCompletes() {
+        stubGetRecipes(Observable.just(RecipeDataFactory.makeRecipesList(3)))
+        val testObserver = getRecipes.buildUseCaseObservable().test()
+        testObserver.assertComplete()
+    }
+
+    @Test
+    fun getRecipesReturnsData() {
+        val recipes = RecipeDataFactory.makeRecipesList(3)
+        stubGetRecipes(Observable.just(recipes))
+        val testObserver = getRecipes.buildUseCaseObservable().test()
+        testObserver.assertValue(recipes)
+    }
+
+    private fun stubGetRecipes(observable: Observable<List<Recipe>>) {
+        whenever(recipesRepository.getRecipes())
+            .thenReturn(observable)
+    }
+
+}
---
+++ Domain/src/test/java/io/android/projectx/domain/test/RecipeDataFactory.kt
@@ -0,0 +1,41 @@
+package io.android.projectx.domain.test
+
+import io.android.projectx.domain.model.Recipe
+import java.time.LocalDate
+import java.util.*
+import java.util.UUID
+import java.util.concurrent.ThreadLocalRandom
+
+
+object RecipeDataFactory {
+
+    fun randomUUID(): String {
+        return UUID.randomUUID().toString()
+    }
+
+    fun randomBoolean(): Boolean {
+        return Math.random() < 0.5
+    }
+
+    fun randomDate(): Date {
+        val random = ThreadLocalRandom.current()
+            .nextLong(LocalDate.of(2016, 1, 1).toEpochDay(), LocalDate.now().toEpochDay())
+        return Date(random)
+    }
+
+    fun uniqueId(): Long {
+        // https://stackoverflow.com/a/41613362/954752
+        return UUID.randomUUID().mostSignificantBits and Long.MAX_VALUE
+    }
+
+    fun makeRecipe(): Recipe {
+        return Recipe(uniqueId(), randomUUID(), randomUUID(), randomUUID(), randomUUID(),
+            randomUUID(), randomDate(), randomUUID())
+    }
+
+    fun makeRecipesList(count:Int):List<Recipe>{
+        val recipes = mutableListOf<Recipe>()
+        repeat(count){recipes.add(makeRecipe())}
+        return recipes
+    }
+}
