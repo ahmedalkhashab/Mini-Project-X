a3582c7a89e2066441d99dd0e1c1c9e667e2ecec
ahmedalkhashab <a.khashab88@gmail.com>
2020-01-31 23:08:26 +0300

    feat(data): Creating the data layer remote data store

---
+++ Data/src/main/java/io/android/projectx/data/store/RecipesRemoteDateStore.kt
@@ -0,0 +1,38 @@
+package io.android.projectx.data.store
+
+import io.android.projectx.data.model.RecipeEntity
+import io.android.projectx.data.repository.RecipesDataStore
+import io.android.projectx.data.repository.RecipesRemote
+import io.reactivex.Completable
+import io.reactivex.Observable
+import javax.inject.Inject
+
+class RecipesRemoteDateStore @Inject constructor(
+    private val recipesRemote: RecipesRemote
+) : RecipesDataStore {
+
+    override fun getRecipes(): Observable<List<RecipeEntity>> {
+        return recipesRemote.getRecipes()
+    }
+
+    override fun clearRecipes(): Completable {
+        throw UnsupportedOperationException("Clearing Recipes isn't supported here...")
+    }
+
+    override fun saveRecipes(recipes: List<RecipeEntity>): Completable {
+        throw UnsupportedOperationException("Saving Recipes isn't supported here...")
+    }
+
+    override fun setRecipeAsBookmarked(recipeId: Long): Completable {
+        throw UnsupportedOperationException("Setting bookmarks isn't supported here...")
+    }
+
+    override fun setRecipeAsNotBookmarked(recipeId: Long): Completable {
+        throw UnsupportedOperationException("Setting bookmarks isn't supported here...")
+    }
+
+    override fun getBookmarkedRecipes(): Observable<List<RecipeEntity>> {
+        throw UnsupportedOperationException("Getting Bookmarked Recipes isn't supported here...")
+    }
+    
+}
