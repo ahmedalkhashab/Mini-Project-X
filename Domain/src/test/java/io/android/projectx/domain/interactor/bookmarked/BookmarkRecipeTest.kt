767cfa4e0d631f51668110bcae8830b770c6861c
ahmedalkhashab <a.khashab88@gmail.com>
2020-01-31 11:57:54 +0300

    test(domain): test 'BookmarkRecipe' UseCase

---
+++ Domain/src/test/java/io/android/projectx/domain/interactor/bookmarked/BookmarkRecipeTest.kt
@@ -0,0 +1,46 @@
+package io.android.projectx.domain.interactor.bookmarked
+
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.whenever
+import io.android.projectx.domain.executor.PostExecutionThread
+import io.android.projectx.domain.repository.RecipesRepository
+import io.android.projectx.domain.test.RecipeDataFactory
+import io.reactivex.Completable
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import java.lang.IllegalArgumentException
+
+class BookmarkRecipeTest {
+
+    private lateinit var bookmarkRecipe: BookmarkRecipe
+    @Mock lateinit var recipesRepository: RecipesRepository
+    @Mock lateinit var postExecutionThread: PostExecutionThread
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        bookmarkRecipe = BookmarkRecipe(recipesRepository,postExecutionThread)
+    }
+
+    @Test
+    fun bookmarkRecipeCompletes(){
+        stubBookmarkRecipe(Completable.complete())
+        val testObserver = bookmarkRecipe.buildUseCaseCompletable(
+            BookmarkRecipe.Params.forRecipe(RecipeDataFactory.uniqueId())
+        ).test()
+        testObserver.assertComplete()
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun bookmarkRecipeThrowException(){
+        bookmarkRecipe.buildUseCaseCompletable().test()
+    }
+
+    private fun stubBookmarkRecipe(completable: Completable){
+        whenever(recipesRepository.bookmarkRecipe(any()))
+            .thenReturn(completable)
+    }
+
+}
