a8a18b89390c52c7a6cb8b66bcdda8380b063c13
ahmedalkhashab <a.khashab88@gmail.com>
2020-02-01 11:10:24 +0300

    test(data): Setting up the tests for the data layer

---
+++ Data/src/test/java/io/android/projectx/data/test/factory/RecipeFactory.kt
@@ -0,0 +1,26 @@
+package io.android.projectx.data.test.factory
+
+import io.android.projectx.data.model.RecipeEntity
+import io.android.projectx.domain.model.Recipe
+
+object RecipeFactory {
+
+    fun makeRecipeEntity(): RecipeEntity {
+        return RecipeEntity(
+            DataFactory.uniqueId(), DataFactory.randomString(),
+            DataFactory.randomString(), DataFactory.randomString(),
+            DataFactory.randomString(), DataFactory.randomString(), DataFactory.randomDate(),
+            DataFactory.randomString()
+        )
+    }
+
+    fun makeRecipe(): Recipe {
+        return Recipe(
+            DataFactory.uniqueId(), DataFactory.randomString(),
+            DataFactory.randomString(), DataFactory.randomString(),
+            DataFactory.randomString(), DataFactory.randomString(), DataFactory.randomDate(),
+            DataFactory.randomString()
+        )
+    }
+
+}
