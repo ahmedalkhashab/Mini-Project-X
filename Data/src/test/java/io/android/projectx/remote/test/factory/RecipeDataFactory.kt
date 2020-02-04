b498af47058ea11493d781fd4a381e8cc070e8da
ahmedalkhashab <a.khashab88@gmail.com>
2020-02-04 21:02:43 +0300

    test(remote): Setting up the Remote layer for testing

---
+++ Data/src/test/java/io/android/projectx/remote/test/factory/RecipeDataFactory.kt
@@ -0,0 +1,48 @@
+package io.android.projectx.remote.test.factory
+
+import io.android.projectx.data.model.RecipeEntity
+import io.android.projectx.remote.model.AuthorModel
+import io.android.projectx.remote.model.RecipeModel
+import io.android.projectx.remote.model.RecipesResponseModel
+
+object RecipeDataFactory {
+
+    fun makeAuthor(): AuthorModel {
+        return AuthorModel(
+            DataFactory.uniqueId(),
+            DataFactory.randomString(),
+            DataFactory.randomString()
+        )
+    }
+
+    fun makeRecipe(): RecipeModel {
+        return RecipeModel(
+            DataFactory.uniqueId(),
+            makeAuthor(),
+            DataFactory.randomString(),
+            DataFactory.randomString(),
+            DataFactory.randomString(),
+            DataFactory.randomString(),
+            DataFactory.randomDate(),
+            DataFactory.randomString()
+        )
+    }
+
+    fun makeRecipeEntity(): RecipeEntity {
+        return RecipeEntity(
+            DataFactory.uniqueId(),
+            DataFactory.randomString(),
+            DataFactory.randomString(),
+            DataFactory.randomString(),
+            DataFactory.randomString(),
+            DataFactory.randomString(),
+            DataFactory.randomDate(),
+            DataFactory.randomString()
+        )
+    }
+
+    fun makeRecipesResponse(): RecipesResponseModel {
+        return RecipesResponseModel(listOf(makeRecipe(), makeRecipe()))
+    }
+
+}
