6a2e123296df77bf53daca21856012e4c7d9bb38
ahmedalkhashab <a.khashab88@gmail.com>
2020-02-04 17:01:50 +0300

    feat(remote): Creating the Retrofit Service Factory for the remote service interface

---
+++ Data/src/main/java/io/android/projectx/remote/service/RecipesServiceFactory.kt
@@ -0,0 +1,43 @@
+package io.android.projectx.remote.service
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import java.util.concurrent.TimeUnit
+
+class RecipesServiceFactory {
+
+    fun makeRecipesService(baseUrl: String, isDebug: Boolean): RecipesService {
+        val okHttpClient = makeOkHttpClient(makeLoggingInterceptor((isDebug)))
+        return makeRecipesService(baseUrl, okHttpClient)
+    }
+
+    private fun makeRecipesService(
+        baseUrl: String,
+        okHttpClient: OkHttpClient
+    ): RecipesService {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(okHttpClient)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+        return retrofit.create(RecipesService::class.java)
+    }
+
+    private fun makeOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .build()
+    }
+
+    private fun makeLoggingInterceptor(isDebug: Boolean): HttpLoggingInterceptor {
+        val logging = HttpLoggingInterceptor()
+        logging.level = if (isDebug) HttpLoggingInterceptor.Level.BODY
+        else HttpLoggingInterceptor.Level.NONE
+        return logging
+    }
+
+}
