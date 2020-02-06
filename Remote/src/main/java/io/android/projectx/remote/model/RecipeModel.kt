97312e7e0d155c7145fd54fe673eeba05853fdb1
ahmedalkhashab <a.khashab88@gmail.com>
2020-02-06 22:58:34 +0300

    refactor(remote): change the recipe model

--- Remote/src/main/java/io/android/projectx/remote/model/RecipeModel.kt
+++ Remote/src/main/java/io/android/projectx/remote/model/RecipeModel.kt
@@ -7,9 +7,9 @@ class RecipeModel(
     @SerializedName("id") val id: Long,
     @SerializedName("author") val author: AuthorModel,
     @SerializedName("title") val title: String,
-    @SerializedName("description") val description: String,
+    @SerializedName("method_description") val description: String,
     @SerializedName("url") val url: String,
     @SerializedName("url_to_image") val urlToImage: String,
     @SerializedName("published_at") val publishedAt: Date,
-    @SerializedName("content") val content: String
+    @SerializedName("ingredients") val content: String
 )
