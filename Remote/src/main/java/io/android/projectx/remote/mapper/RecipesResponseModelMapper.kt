7936d1289701d0fa0ac7b17f452e18ebc3091f8f
ahmedalkhashab <a.khashab88@gmail.com>
2020-02-08 23:07:50 +0300

    fix(RecipesResponseModelMapper): add "Inject"

--- Remote/src/main/java/io/android/projectx/remote/mapper/RecipesResponseModelMapper.kt
+++ Remote/src/main/java/io/android/projectx/remote/mapper/RecipesResponseModelMapper.kt
@@ -2,8 +2,9 @@ package io.android.projectx.remote.mapper
 
 import io.android.projectx.data.model.RecipeEntity
 import io.android.projectx.remote.model.RecipeModel
+import javax.inject.Inject
 
-open class RecipesResponseModelMapper :
+open class RecipesResponseModelMapper @Inject constructor() :
     ModelMapper<RecipeModel, RecipeEntity> {
 
     override fun mapFromModel(model: RecipeModel): RecipeEntity {
