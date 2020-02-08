1efaae2df71a2da4c4184a07479c53da2fb8d93e
ahmedalkhashab <a.khashab88@gmail.com>
2020-02-08 13:41:49 +0300

    feat(presentation): Creating the Post Execution Thread Implementation

---
+++ Presentation/src/main/java/io/android/projectx/presentation/UiThread.kt
@@ -0,0 +1,12 @@
+package io.android.projectx.presentation
+
+import io.android.projectx.domain.executor.PostExecutionThread
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import javax.inject.Inject
+
+class UiThread @Inject constructor(): PostExecutionThread{
+
+    override val scheduler: Scheduler
+        get() = AndroidSchedulers.mainThread()
+}
