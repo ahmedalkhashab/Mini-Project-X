8345ec7cc7cfe6754c7fc5ce33ecbf5fd8d771de
ahmedalkhashab <a.khashab88@gmail.com>
2020-02-09 01:02:08 +0300

    fix: wrong attributes

--- Remote/src/main/java/io/android/projectx/remote/model/AuthorModel.kt
+++ Remote/src/main/java/io/android/projectx/remote/model/AuthorModel.kt
@@ -3,7 +3,7 @@ package io.android.projectx.remote.model
 import com.google.gson.annotations.SerializedName
 
 class AuthorModel(
-    @SerializedName("author_id") val authorId: Long,
+    @SerializedName("id") val authorId: Long,
     @SerializedName("author_name") val authorName: String,
-    @SerializedName("author_avatar_url") val authorAvatar: String
+    @SerializedName("author_avatar") val authorAvatar: String
 )
