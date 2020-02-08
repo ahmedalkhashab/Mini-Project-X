2a97e4ae197a85f900a56f905ddba981dec64810
ahmedalkhashab <a.khashab88@gmail.com>
2020-02-09 02:43:27 +0300

    test(presentation): Writing Espresso tests for the Browse Recipes Activity

---
+++ Presentation/src/androidTest/java/io/android/projectx/presentation/features/browse/BrowseProjectsActivityTest.kt
@@ -0,0 +1,52 @@
+package io.android.projectx.presentation.features.browse
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import com.nhaarman.mockitokotlin2.whenever
+import io.android.projectx.domain.model.Recipe
+import io.android.projectx.presentation.R
+import io.android.projectx.presentation.test.TestApplication
+import io.android.projectx.presentation.test.factory.RecipeDataFactory
+import io.reactivex.Observable
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class BrowseRecipesActivityTest {
+
+    @Rule
+    @JvmField
+    val activity = ActivityTestRule<BrowseActivity>(BrowseActivity::class.java, false, false)
+
+    @Test
+    fun activityLaunches() {
+        stubRecipesRepositoryGetRecipes(Observable.just(listOf(RecipeDataFactory.makeRecipe())))
+        activity.launchActivity(null)
+    }
+
+    @Test
+    fun recipesDisplay() {
+        val recipes = listOf(RecipeDataFactory.makeRecipe(),
+            RecipeDataFactory.makeRecipe(), RecipeDataFactory.makeRecipe())
+        stubRecipesRepositoryGetRecipes(Observable.just(recipes))
+        activity.launchActivity(null)
+
+        recipes.forEachIndexed { index, recipe ->
+            onView(withId(R.id.recyclerRecipes))
+                .perform(RecyclerViewActions.scrollToPosition<BrowseAdapter.ViewHolder>(index))
+
+            onView(withId(R.id.recyclerRecipes))
+                .check(matches(hasDescendant(withText(recipe.title))))
+        }
+    }
+
+    private fun stubRecipesRepositoryGetRecipes(observable: Observable<List<Recipe>>) {
+        whenever(TestApplication.appComponent().recipesRepository().getRecipes())
+            .thenReturn(observable)
+    }
+}
