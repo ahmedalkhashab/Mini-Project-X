9facd9845d74b64278bbd317b5ce434287bc4055
ahmedalkhashab <a.khashab88@gmail.com>
2020-02-13 16:30:47 +0300

    feat: build Glide Module

--- Presentation/src/main/java/io/android/projectx/presentation/di/module/ApplicationModule.kt
+++ Presentation/src/main/java/io/android/projectx/presentation/di/module/ApplicationModule.kt
@@ -2,12 +2,39 @@ package io.android.projectx.presentation.di.module
 
 import android.app.Application
 import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
+import com.bumptech.glide.request.RequestOptions
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
+import io.android.projectx.presentation.R
 
-@Module
+@Module(includes = [GlideModule::class])
 abstract class ApplicationModule {
 
     @Binds
     abstract fun bindContext(application: Application): Context
+}
+
+@Module
+internal object GlideModule {
+
+    @Provides
+    @JvmStatic
+    fun provideRequestOptions(): RequestOptions {
+        return RequestOptions
+            .placeholderOf(R.drawable.ic_placeholder)
+            .error(R.drawable.ic_error_background)
+    }
+
+    @Provides
+    @JvmStatic
+    fun provideGlideInstance(
+        application: Application,
+        requestOptions: RequestOptions
+    ): RequestManager {
+        return Glide.with(application).setDefaultRequestOptions(requestOptions)
+    }
+
 }
