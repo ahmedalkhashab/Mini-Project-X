c73062bf51322b2aeaeea48c0ef9dc6027af7536
ahmedalkhashab <a.khashab88@gmail.com>
2020-02-23 17:37:37 +0300

    chore: prepare ci server

--- setup_export.sh
+++ setup_export.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 #for dev-release apk
-LATEST_APK_DEV=$(ls -lrt ./app/build/outputs/apk/dev/release/*.apk | tail -1 | awk -F" " '{ print $9 }') #Pick the latest dev build apk.
+LATEST_APK_DEV=$(ls -lrt ./Presentation/build/outputs/apk/dev/release/*.apk | tail -1 | awk -F" " '{ print $9 }') #Pick the latest dev build apk.
 FILE_NAME_DEV=$(basename $LATEST_APK_DEV .apk)".apk"
 FILE_TITLE_DEV=$(basename $LATEST_APK_DEV .apk) #optional -- For changelog title.
 
 #for Staging-release apk
-LATEST_APK_STAGING=$(ls -lrt ./app/build/outputs/apk/staging/release/*.apk | tail -1 | awk -F" " '{ print $9 }') #Pick the latest Staging build apk.
+LATEST_APK_STAGING=$(ls -lrt ./Presentation/build/outputs/apk/staging/release/*.apk | tail -1 | awk -F" " '{ print $9 }') #Pick the latest Staging build apk.
 FILE_NAME_STAGING=$(basename $LATEST_APK_STAGING .apk)".apk"
 FILE_TITLE_STAGING=$(basename $LATEST_APK_STAGING .apk) #optional -- For changelog title.
 
 #for production-release apk
-LATEST_APK_PRODUCTION=$(ls -lrt ./app/build/outputs/apk/production/release/*.apk | tail -1 | awk -F" " '{ print $9 }') #Pick the latest production build apk.
+LATEST_APK_PRODUCTION=$(ls -lrt ./Presentation/build/outputs/apk/production/release/*.apk | tail -1 | awk -F" " '{ print $9 }') #Pick the latest production build apk.
 FILE_NAME_PRODUCTION=$(basename $LATEST_APK_PRODUCTION .apk)".apk"
 FILE_TITLE_PRODUCTION=$(basename $LATEST_APK_PRODUCTION .apk) #optional -- For changelog title.
 
